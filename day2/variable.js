643c3d87da1603ea59f3dc83de74b7b84e617c45
shyam-sm <shyammali1105@gmail.com>
2026-04-15 02:44:22 +0000

    variables - var let const

---
+++ day2/variable.js
@@ -0,0 +1,13 @@
+const accountNo = 1238;
+var  name = "hitesh";
+let password = "34g567";
+city = "mumbai";
+
+{
+    //accountNo = 45678;
+    name = "raj";
+    password = "123456";
+    city = "udaipur";
+}
+
+console.table([accountNo, name, password, city])
