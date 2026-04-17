d754999c4e9162fdf53ed84569182351c1c15761
shyam-sm <shyammali1105@gmail.com>
2026-04-17 08:13:25 +0530

    comparsion of different datatypes

---
+++ day2/04_comparison.js
@@ -0,0 +1,34 @@
+//lets strt with basic comparison (number conversions)
+
+// console.log( 2 > 1 );
+// console.log( 2 < 1 );
+// console.log( 2 >= 1 );
+// console.log( 2 >= 1 );
+// console.log( 2 == 1 );
+// console.log( 2 != 1 );
+
+// console.log( "0002" > 1.9 )
+
+// console.log( null >0 )          //false
+// console.log( null ==0 )         //false
+// console.log( null >=0 )         //true
+
+
+// console.log( null <0 )          //false
+// console.log( null ==0 )         //false
+// console.log( null <=0 )         //true
+// console.log( null !=0 )         //true
+
+
+console.log( NaN >0 )          //false
+console.log( NaN ==0 )         //false
+console.log( NaN >=0 )         //false
+console.log( undefined >=0 )         //false
+console.log( undefined ==0 )         //false
+console.log( undefined >=0 )         //false
+
+
+
+/* String vs String comparison */
+
+console.log("abddddd">"aeaa")
